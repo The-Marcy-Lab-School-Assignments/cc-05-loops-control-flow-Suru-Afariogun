9756ffc917f271ad960435334291d6d0768beeb2
Suru-Afariogun <suruafa@gmail.com>
2024-10-09 11:02:19 -0400

    I got to question 2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+//1
+const multiplesOfSixAndNine = () => {
+  for (i = 1; i <= 100; i++) {
+    if (i % 6 === 0 && i % 9 === 0) {
+      console.log(i);
+    }
+  }
+};
+multiplesOfSixAndNine();
+//2
+const oddAndEvenToN = (num) => {
+  for (i = 0; i < num; i++) {
+    console.log(i);
+    if (i % 2 === 0) {
+      console.log("even");
+    } else {
+      console.log("odd");
+    }
+  }
+};
 
+oddAndEvenToN(5);
+//3
+const sumOfNotDivisibleByTen = () =>{
+    for()
+}
